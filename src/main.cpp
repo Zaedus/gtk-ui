2e0bb4d3693e757055978f9337a1303483c97816
Zaedus <nintendozaedus@gmail.com>
2022-01-15 22:16:54 -0600

    Define option types and handle errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "cxxopts.hpp"
+#include "colors.hpp"
 
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
 
 int main(int argc, char **argv)
 {
@@ -15,17 +18,20 @@ int main(int argc, char **argv)
 
     options.add_options()
         ("h,help", "Print usage")
-        ("o,output", "Output file");
+        ("i,input", "Input file", cxxopts::value<string>())
+        ("o,output", "Output file", cxxopts::value<string>());
 
-    // Parse options
-    auto result = options.parse(argc, argv);
+    try {
+        // Parse options
+        auto result = options.parse(argc, argv);
 
-    // Show help dialog
-    if (result.count("help") || result.arguments().size() == 0)
-    {
-        cout << options.help() << endl;
-        exit(EXIT_SUCCESS);
+        // Show help dialog
+        if (result.count("help") || result.arguments().size() == 0)
+        {
+            cout << options.help() << endl;
+            exit(EXIT_SUCCESS);
+        }
+    } catch (const cxxopts::OptionException& e) {
+        cout << COLOR_ERROR << "error: " << COLOR_RESET << e.what() << endl;
     }
-
-    
 }
