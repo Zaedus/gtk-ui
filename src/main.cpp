9f7351b59418c5f772d70e1170cc9599b9336025
Zaedus <nintendozaedus@gmail.com>
2022-01-15 22:06:57 -0600

    Setup basic option parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
+#include "cxxopts.hpp"
+
 #include <iostream>
 
-int main()
+using std::cout;
+using std::endl;
+
+int main(int argc, char **argv)
 {
-    std::cout << "Test" << std::endl;
+    // Define options
+    cxxopts::Options options{"gtkui", "Transpiler for the Gtk UI language"};
+
+    options.positional_help("[INPUT]");
+    options.parse_positional({"input"});
+
+    options.add_options()
+        ("h,help", "Print usage")
+        ("o,output", "Output file");
+
+    // Parse options
+    auto result = options.parse(argc, argv);
+
+    // Show help dialog
+    if (result.count("help") || result.arguments().size() == 0)
+    {
+        cout << options.help() << endl;
+        exit(EXIT_SUCCESS);
+    }
+
+    
 }
