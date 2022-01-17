cd65af045d022902f1aa806b2e58bd77c26446ad
Zaedus <nintendozaedus@gmail.com>
2022-01-16 20:05:07 -0600

    Print arrow pointing to error

--- src/common.cpp
+++ src/common.cpp
@@ -2,6 +2,7 @@
 #include "colors.hpp"
 
 #include <iostream>
+#include <fmt/format.h>
 
 using std::string;
 using std::cout;
@@ -16,6 +17,7 @@ void fail(string reason)
 void fail_line(string reason, string &line, string &filename, size_t &line_number, size_t &line_char)
 {
     cout << COLOR_BOLD << filename << ":" << line_number << ":" << line_char << ": " << COLOR_ERROR << "error: " << COLOR_BOLD << reason << COLOR_RESET << endl
-         << "\t" << line << endl;
+         << "\t" << line << endl
+         << "\t" << fmt::format("{}{:>{}}", COLOR_GREEN, "^", line_char + 1) << endl;
     exit(EXIT_FAILURE);
 }
