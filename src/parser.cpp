663c231bda323a036f43b56f7b09994107e2aeaf
Zaedus <nintendozaedus@gmail.com>
2022-01-16 18:55:05 -0600

    Handle parsing state and parse directives

--- src/parser.cpp
+++ src/parser.cpp
@@ -8,17 +8,58 @@ using std::vector;
 using std::getline;
 using std::ios;
 
-Parser::Parser(string input_path)
-: stream{input_path, ios::in}
+namespace fs = std::filesystem;
+
+Parser::Parser(string input_path, vector<string> libs_path)
+: stream{input_path, ios::in},
+  state{ParsingState::DEFAULT}
 {
     if (!stream.is_open())
         throw std::runtime_error{"File doesn't exist"};
+    
+    for (string &path : libs_path)
+    {
+        if (fs::is_directory(path)) libs.push_back(path);
+    }
 }
 
 void Parser::parse(string root_element, vector<string> args)
 {
     string line;
     while(getline(stream, line)) {
-        cout << line << endl;
+        if (state == ParsingState::DIRECTIVE) state = ParsingState::DEFAULT;
+
+        for (size_t i = 0; i < line.length(); ++i) {
+            switch (line[i])
+            {
+                case '\t':
+                case ' ':
+                    continue;
+                case '#':
+                    ++i;
+                    state = ParsingState::DIRECTIVE;
+                    
+                    string substr = line.substr(i, line.find(' ', i) - i);
+                    if (substr == "include")
+                    {
+                        i += 8;
+
+                        string name;
+                        i += parse_string(line, i, name);
+                    }
+                    continue;
+            }
+            cout << line[i];
+        }
+        cout << endl;
     }
+}
+
+size_t Parser::parse_string(std::string input, size_t position, std::string &result)
+{
+    size_t start_pos = input.find('"', position) + 1;
+    size_t end_pos = input.find('"', start_pos);
+    result = input.substr(start_pos, end_pos - start_pos);
+
+    return end_pos - start_pos + 1;
 }
--- src/parser.hpp
+++ src/parser.hpp
@@ -4,13 +4,25 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <filesystem>
+
+enum ParsingState {
+    DEFAULT,
+    DIRECTIVE,
+    DEFINITION,
+    ENUM
+};
 
 class Parser
 {
 public:
-    Parser(std::string input_file);
+    Parser(std::string input_file, std::vector<std::string> libs_path);
     void parse(std::string root_element, std::vector<std::string> arguments);
 
 private:
+    size_t parse_string(std::string input, size_t position, std::string &result);
+
     std::fstream stream;
+    std::vector<std::string> libs;
+    ParsingState state;
 };
