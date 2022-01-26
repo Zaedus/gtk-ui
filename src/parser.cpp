e5e5a7c1a71fa9b1710beeb7287444063ba8734e
Zaedus <nintendozaedus@gmail.com>
2022-01-26 12:46:32 -0600

    Setup numeric literal

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,15 +2,37 @@
 #include "common.hpp"
 #include "tokenizer.hpp"
 
+#include <fstream>
+#include <fmt/format.h>
+
 using std::string;
 using std::vector;
+using std::ifstream;
 
 Parser::Parser(string file, vector<string> libs)
 {
-    
+    ifstream ifs(file);
+    if(ifs.is_open())
+    {
+        str = string(
+            (std::istreambuf_iterator<char>(ifs)),
+            (std::istreambuf_iterator<char>())
+        );
+    }
+    else throw std::runtime_error{fmt::format("File '{}' not found", file)};
 }
 
-void Parser::parse()
+Token Parser::parse()
 {
-    
+    return program();
+}
+
+Token Parser::program()
+{
+    return numberic_literal();
+}
+
+Token Parser::numberic_literal()
+{
+    return Token(std::stol(str));
 }
--- src/parser.hpp
+++ src/parser.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
+#include "token.hpp"
+
 #include <string>
 #include <vector>
 
 class Parser
 {
+private:
+    std::string str;
+
 public:
     Parser(std::string file, std::vector<std::string> libs);
 
-    void parse();
+    Token parse();
+
+    /**
+     * Components
+     */
+
+    Token program();
+    Token numberic_literal();
 };
